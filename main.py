c14fe24fd463be907e867f1b641ae0e5a9851cb0
8an4anam1ni0n <yoshuamalicea@gmail.com>
2022-01-06 18:12:46 -0500

    Added solution part1 of day 2

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from solutions import Day1
+# from solutions import Day1
+from solutions import Day2
 
 test_path = "./test/{}.txt"
 input_path = "./input/{}.txt"
 
 def main() -> None:
   print('Hello AOC')
-  print(Day1.solution(input_path.format('day_1')))
+  # print(Day1.solution(input_path.format('day_1')))
+  print(Day2.solution(input_path.format('day_2')))
 
 
 if __name__ == '__main__':
--- solutions/Day2/solution.py
+++ solutions/Day2/solution.py
@@ -1,2 +1,51 @@
 from typing import Protocol
 from ..Utilities import read_input_file
+from dataclasses import dataclass
+
+@dataclass
+class Submarine:
+    horizontal: int = 0
+    depth: int = 0
+
+class Command(Protocol):
+    def execute(self, submarine: Submarine) -> None:
+        ...
+
+class ForwardCommand:
+    def __init__(self, moves: int) -> None:
+        self.moves = moves
+
+    def execute(self, submarine: Submarine) -> None:
+        submarine.horizontal += self.moves
+
+class UpCommand:
+    def __init__(self, moves: int) -> None:
+        self.moves = moves
+
+    def execute(self, submarine: Submarine) -> None:
+        submarine.depth -= self.moves
+
+class DownCommand:
+    def __init__(self, moves: int) -> None:
+        self.moves = moves
+
+    def execute(self, submarine: Submarine) -> None:
+        submarine.depth += self.moves
+
+def parse_data(data: list[str]):
+    return [{
+        'forward': ForwardCommand,
+        'up': UpCommand,
+        'down': DownCommand
+    }[command](int(moves)) for command, moves in [d.split() for d in data]]
+
+
+def part1(commandlist: list[Command]) -> int:
+    submarine = Submarine()
+    for command in commandlist:
+        command.execute(submarine)
+    return submarine.horizontal * submarine.depth
+
+def solution(filename: str) -> None:
+    commandlist = parse_data(read_input_file(filename))
+    print(f'Part 1: {part1(commandlist)}')
