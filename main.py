dace6c0733a6097a241b52c315163656370acf13
Yoshuam Alicea <yoshuamalicea@gmail.com>
2022-01-07 10:43:17 -0500

    Added solution part1 day 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from solutions import Day1
-from solutions import Day2
+from solutions import Day2, Day3
 
 test_path = "./test/{}.txt"
 input_path = "./input/{}.txt"
@@ -7,6 +7,7 @@ input_path = "./input/{}.txt"
 def main() -> None:
   print(Day1.solution(input_path.format('day_1')))
   print(Day2.solution(input_path.format('day_2')))
+  print(Day3.solution(input_path.format('day_3')))
 
 
 if __name__ == '__main__':
--- solutions/Day3/solution.py
+++ solutions/Day3/solution.py
@@ -1,6 +1,6 @@
+from ..Utilities import read_input_file
 from functools import reduce
 from typing import Callable
-from ..Utilities import read_input_file
 
 
 def transpose(data: list[str]) -> list[str]:
@@ -12,26 +12,36 @@ def is_greater_than(x, y):
 def is_less_than(x, y):
     return x < y
 
-def get_common_bit_helper(bitstream: str, commonness: Callable) -> int:
-    return 1 if commonness(bitstream.count('1'), len(bitstream) / 2) else 0
+def get_common_bit_helper(bitstream: str, commonness: Callable) -> str:
+    return '1' if commonness(bitstream.count('1'), len(bitstream) / 2) else '0'
 
-def get_most_common_bit(bitstream: str) -> int:
+def get_most_common_bit(bitstream: str) -> str:
     return get_common_bit_helper(bitstream, is_greater_than)
 
-def get_least_common_bit(bitstream: str) -> int:
+def get_least_common_bit(bitstream: str) -> str:
     return get_common_bit_helper(bitstream, is_less_than)
 
-def calculate_gamma_rate(data: list[str]) -> int:
-    return reduce(lambda x, y: (x << 1) | get_most_common_bit(y), data, 0)
+def calculate_gamma_rate(data: list[str]) -> str:
+    return ''.join(get_most_common_bit(bitstream) for bitstream in data)
 
-def calculate_epsilon_rate(gamma_rate: int) -> int:
-    return gamma_rate ^ int('1' * bin(gamma_rate)[2:], 2)
+def calculate_epsilon_rate(gamma_rate: int) -> str:
+    bit_length = len(gamma_rate)
+    print(bit_length)
+    return bin(int(gamma_rate, 2) ^ int('1' * bit_length, 2))[2:]
 
-def part1():
-    pass
+def calculate_power_consumption(data: list[str]) -> int:
+    gamma_rate = calculate_gamma_rate(data)
+    return int(gamma_rate, 2) * int(calculate_epsilon_rate(gamma_rate),2)
+
+def part1(data: list[str]):
+    return calculate_power_consumption(data)
 
 def part2():
     pass
 
 def solution(filename):
-    pass
+    data = transpose(read_input_file(filename))
+    print(f"====== Solution Day 3 ======")
+    print(f'Part 1: {part1(data)}')
+    # print(f'Part 2: {part2(commandlist)}')
+    print(f"============================")
