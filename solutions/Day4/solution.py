3828d76145a3c3f2773be152c952345affaa6b9c
yoshuamalicea <yoshuamalicea@gmail.com>
2022-01-12 18:41:48 +0000

    Added day 4 directory for solution day 4

---
+++ solutions/Day4/solution.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from ..Utilities import read_input_file
+
+
+def part1() -> None:
+    pass
+
+def part2() -> None:
+    pass
+
+def main() -> None:
+    pass
+
+
+
+if __name__ == '__main__':
+    pass
