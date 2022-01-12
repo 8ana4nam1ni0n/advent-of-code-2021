c602f3f8b1cf8d940847afbf89d22e39ef4200bb
yoshuamalicea <yoshuamalicea@gmail.com>
2022-01-12 22:34:18 +0000

    Started solution day 4

--- solutions/Day4/solution.py
+++ solutions/Day4/solution.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
-from ..Utilities import read_input_file
+from ..Utilities import read_input_file_raw
+
+def parse_data(data: str) -> tuple[list[str], list[str]]:
+    numbers, *cards = data.split('\n\n')
+    # TODO Transpose cards rows and make it an array of array of dicts
+    return numbers.split(','), [ [ [int(x) for x in row.split()] for row in card.splitlines()] for card in cards]
 
 
 def part1() -> None:
@@ -8,8 +13,9 @@ def part1() -> None:
 def part2() -> None:
     pass
 
-def main() -> None:
-    pass
+def solution(filename: str) -> None:
+    data = read_input_file_raw(filename)
+
 
 
 
