00047bcf6487d09de7bb3d6215418fb606b94c4f
yoshuamalicea <yoshuamalicea@gmail.com>
2022-01-13 16:08:00 +0000

    continued on day 4 solution

--- solutions/Day4/solution.py
+++ solutions/Day4/solution.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 from ..Utilities import read_input_file_raw
 
-def parse_data(data: str) -> tuple[list[str], list[str]]:
-    numbers, *cards = data.split('\n\n')
-    # TODO Transpose cards rows and make it an array of array of dicts
-    return numbers.split(','), [ [ [int(x) for x in row.split()] for row in card.splitlines()] for card in cards]
+def parse_data(data: str) -> tuple[list[str], list[dict[str, int]]]:
+    numbers, *cards_data = data.split('\n\n')
+    cards = [card_data.splitlines() for card_data in cards_data]
+    transposed_cards = [list(zip(*[row.split() for row in card])) for card in cards]
+    return numbers.split(','), [[{k:0 for k in row} for row in tcard] for tcard in transposed_cards]
 
+class BingoCard:
+    def __init__(self, card_data):
+        self.card = {key:value for key, value in zip('bingo', card_data)}
+    
+    #TODO try to implement observer pattern for easy winner notification
+
+        
 
 def part1() -> None:
     pass
